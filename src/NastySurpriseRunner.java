6946b579b48cb2bae3697b09b9ad95a2ecf73ef2
League Student <league@iMac-11.attlocal.net>
2019-09-22 17:27:16 -0700

    Started nasty surprise runner

---
+++ src/NastySurpriseRunner.java
@@ -0,0 +1,19 @@
+import java.awt.Frame;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class NastySurpriseRunner {
+	JFrame Frame = new JFrame();
+	JPanel Panel = new JPanel();
+	JButton left = new JButton();
+	JButton Right = new JButton();
+	
+
+
+	
+public static void main(String[] args) {
+NastySurpriseRunner hi = new NastySurpriseRunner();								
+}
+}
